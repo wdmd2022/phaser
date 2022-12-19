9ece59064abb073a7686b2677b541a96f5d51eaf
Douglas <wdmdavison@icloud.com>
2022-12-19 13:57:48 -0800

    upload for testing

--- js/SceneTitle.js
+++ js/SceneTitle.js
@@ -3,7 +3,7 @@ class SceneTitle extends Phaser.Scene {
         super('SceneTitle');
     }
     preload() {
-        this.preload.image("startButton", "assets/Images/start.png");
+        this.load.image("startButton", "assets/Images/start.png");
     }
     create() {
         console.log('SceneTitle');

86f83488231245b4a8824ec4785ab1aa618c58bd
Douglas <wdmdavison@icloud.com>
2022-12-19 14:03:33 -0800

    upload smaller button

--- js/SceneTitle.js
+++ js/SceneTitle.js
@@ -3,7 +3,7 @@ class SceneTitle extends Phaser.Scene {
         super('SceneTitle');
     }
     preload() {
-        this.load.image("startButton", "assets/Images/startbutton.png");
+        this.load.image("startButton", "assets/Images/startbut.png");
     }
     create() {
         console.log('SceneTitle');

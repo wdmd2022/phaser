2b4940b8d0ba870e1e893b9f1d7ca05fff0c86fe
Douglas <wdmdavison@icloud.com>
2022-12-19 15:10:59 -0800

    upload with bubble

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -2,9 +2,11 @@ class SceneMain extends Phaser.Scene {
     constructor() {
         super('SceneMain');
     }
+
     preload() {
         this.load.image('background', '/assets/Images/back-ground.png');
-        this.load.audio('backgroundMusic', '/assets/Sound_Effects/Adventure-320bit.mp3')
+        this.load.audio('backgroundMusic', '/assets/Sound_Effects/Adventure-320bit.mp3');
+        this.load.spritesheet('bubble', '/assets/Images/bub.png', { frameWidth: 100, frameHeight: 100 })
       }
       
       
@@ -17,9 +19,49 @@ class SceneMain extends Phaser.Scene {
         enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
         backgroundmusic = this.sound.add('backgroundMusic');
         backgroundmusic.play();
+        bubbleToGet = this.physics.add.sprite(200, 150, 'bubble');
+        bubbleToGet.setCollideWorldBounds(true);
       }
       
     update() {
-      
-      }
+        let gameover = false;
+        if(gameover){
+            return;
+        }
+    
+        if(bubbleToGet.x >= 750){
+            bubbleToGet.x = 749;
+            bubbleToGetXSpeed = bubbleToGetXSpeed * (-1);
+            bubbleToGet.setVelocityX(bubbleToGetXSpeed);
+        } else {
+            bubbleToGet.x = bubbleToGet.x + bubbleToGetXSpeed;
+        }
+    
+    
+        if(bubbleToGet.x <= 50){
+            bubbleToGet.x = 51;
+            bubbleToGetXSpeed = bubbleToGetXSpeed * (-1);
+            bubbleToGet.setVelocityX(bubbleToGetXSpeed);
+        } else {
+            bubbleToGet.x = bubbleToGet.x + bubbleToGetXSpeed;
+        }
+    
+    
+        if(bubbleToGet.y >= 550){
+            bubbleToGet.y = 549;
+            bubbleToGetYSpeed = bubbleToGetYSpeed * (-1);
+            bubbleToGet.setVelocityY(bubbleToGetYSpeed);
+        } else {
+            bubbleToGet.y = bubbleToGet.y + bubbleToGetYSpeed;
+        }
+    
+    
+        if(bubbleToGet.y <= 50){
+            bubbleToGet.y = 51;
+            bubbleToGetYSpeed = bubbleToGetYSpeed * (-1);
+            bubbleToGet.setVelocityY(bubbleToGetYSpeed);
+        } else {
+            bubbleToGet.y = bubbleToGet.y + bubbleToGetYSpeed;
+        }
+    }
 }
